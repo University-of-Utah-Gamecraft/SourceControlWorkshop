9af164227579c5cd29137aa692cf176dec195715
Ethan Boggs <ethanbeez@gmail.com>
2022-10-28 12:41:35 -0600

    First main commit of feature scenes 1 and 2, as well as feature scripts 1 and 2!

---
+++ Assets/Scripts/Feature1Script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is attached to the Feature1 object in Scene 1.
+/// </summary>
+public class Feature1Script : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Hi from feature 1! (:");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/Feature2Script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is attached to the Feature2 object in Scene 2.
+/// </summary>
+public class Feature2Script : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Hi from feature 2! (:");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
