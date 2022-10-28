d432c852fe1dd0bebc3def6f4d5f25d1248c540b
Ethan Boggs <ethanbeez@gmail.com>
2022-10-28 17:31:32 -0600

    I made some cool changes!

--- Assets/Scripts/Feature1Script.cs
+++ Assets/Scripts/Feature1Script.cs
@@ -10,6 +10,7 @@ public class Feature1Script : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         Debug.Log("Hi from feature 1! (:");
+        Debug.Log("Hello!!!!!!!");
     }
 
     // Update is called once per frame
